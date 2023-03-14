28579d6b330fc174971555b17246c1ed642e4622
Doug <dmhwebb@gmail.com>
2023-03-14 14:08:46 +0100

    works

--- src/main.ts
+++ src/main.ts
@@ -1,127 +1,106 @@
 import './style.css'
 
-const boardRoot = 4;
-
-type Coordinates = [number, number];
-
 interface Player {
-    name: string,
+    name: string;
     symbol: "x" | "o";
+    cells: string[];
     score: number;
 };
 
-interface CellState {
-    element: Element;
-    markedBy: string | null;
-};
+type Coordinates = [number, number];
+
+const players: Array<Player> = [
+    {name: "player1", symbol: "x", cells: [], score: 0},
+    {name: "player2", symbol: "o", cells: [], score: 0}
+];
+
+const tttCellStyles = ["flex", "items-center", "justify-center", "bg-emerald-300", "w-32", "h-32", "border-black", "border-solid", "border-2", "text-3xl", "font-bold"];
+
+const tttHolderStyles = ["grid", "grid-cols-3", "mt-10"];
+
+const winConditions = [
+    ["0-0", "0-1", "0-2"],
+    ["1-0", "1-1", "1-2"],
+    ["2-0", "2-1", "2-2"],
+    ["0-0", "1-0", "2-0"],
+    ["0-1", "1-1", "2-1"],
+    ["0-2", "1-2", "2-2"],
+    ["0-0", "1-1", "2-2"],
+    ["0-2", "1-1", "2-0"]
+];
+
+const boardRoot = 3;
+
+let playIndex = 0;
+
+let currentPlayer = 0;
+
+let currentlyPlaying = true;
 
 function coordToId(coord: Coordinates): `${number}-${number}` {
-    // array destructuring
     const [row, col] = coord;
-    // long form
-    // const row = coord[0];
-    // const col = coord[1];
     return `${row}-${col}`;
-}
-
-function idToCoord(id: string): Coordinates {
-    return [+id[0], +id[2]];
 }
 
 function tttBoardCreate(boardRoot: number) {
     const tttHolder = document.createElement("div");
     tttHolder.id = "tttHolder";
-    tttHolder.classList.add("grid", "grid-cols-4", "mt-10");
-    tttHolder?.addEventListener("click", play)
+    tttHolder.classList.add(...tttHolderStyles);
+    tttHolder.addEventListener("click", play);
     document.querySelector("h1")?.after(tttHolder);
-
     for (let row = 0; row < boardRoot; row++) {
         for (let col = 0; col < boardRoot; col++) {
-            let cell = document.createElement("div");
-            cell.classList.add("flex", "items-center", "justify-center", "bg-emerald-300", "w-32", "h-32", "border-black", "border-solid", "border-2", "font-xl", "font-bold");
-            cell.id = coordToId([row, col]);
-            document.getElementById("tttHolder")?.appendChild(cell);
-        }
-    }
-}
-
-let playIndex = 0;
-
-function play(e: Event | any) {
-    if (e.target.innerHTML == "") {
-        if (playIndex % 2 == 0) {
-            e.target.innerHTML = "X";
-            playIndex++;
-            if (playIndex > 4) {
-                checkForWin(e, "X");
-                console.log("I'm checking...")
-            };
-        } else if (playIndex % 2 == 1) {
-            e.target.innerHTML = "O"
-            playIndex++;
-            if (playIndex > 4) {
-                checkForWin(e, "O");
-                console.log("I'm checking...")
-
-            };
+            let tttCell = document.createElement("div");
+            tttCell.classList.add(...tttCellStyles);
+            tttCell.id = coordToId([row, col]);
+            document.getElementById("tttHolder")?.appendChild(tttCell);
         };
     };
 };
 
-const checkDirections = [
-    [-1, -1],
-    [ 0,  1],
-    [ 1,  1],
-    [ 1,  0],
-    [ 1, -1],
-    [ 0, -1],
-    [-1, -1],
-    [-1,  0]
-];
+function play(e: Event) {
+    if (currentlyPlaying === true) {
+        const input = e.target as HTMLElement;
+        if (input.innerHTML == "") {
+            input.innerHTML = players[currentPlayer].symbol;
+            const targetId: string = input.id;
+            players[currentPlayer].cells.push(targetId);
+            if (playIndex > 3) {
+                checkForWin();
+            };
+            currentPlayer = (currentPlayer + 1) % players.length;
+            playIndex++;
+        };
+    };
+};
 
-function checkForWin(e:Event, playerSymbol:string): boolean {
-    const startPoint = idToCoord(e.target?.id);
-    console.log(playerSymbol, idToCoord(e.target?.id))
-    function checkFrom(point: Coordinates): boolean {
-        for (let i = 0; i < checkDirections.length; i++) {
-            const checkPoint = 
-                [
-                    (point[0] + checkDirections[i][0]), 
-                    (point[1] + checkDirections[i][1])
-                ];
-            if (
-                    checkPoint[0] >= 0 && 
-                    checkPoint[0] <= boardRoot - 1 && 
-                    checkPoint[1] >= 0 && 
-                    checkPoint[1] <= boardRoot - 1 
-                ) {
-                    if (document.getElementById(coordToId(checkPoint))?.innerHTML === playerSymbol) {
-                        const checkPoint2 = 
-                        [
-                            (checkPoint[0] + checkDirections[i][0]), 
-                            (checkPoint[1] + checkDirections[i][1])
-                        ];
-                        if (document.getElementById(coordToId(checkPoint2))?.innerHTML === playerSymbol) {
-                            playIndex = 0;
-                            console.log(playIndex, "WINNNNNNNNN!!!!!");
-                            return true;
-                        }
-                    }
-                }
-                else {
-                    return false;
-                }
-            }
-        }
-    checkFrom(startPoint);
+function checkForWin(): boolean {
+    for (const winCondition of winConditions) {
+        const [winCell1, winCell2, winCell3] = [...winCondition];
+        if (players[currentPlayer].cells.includes(winCell1) === true &&
+            players[currentPlayer].cells.includes(winCell2) === true &&
+            players[currentPlayer].cells.includes(winCell3) === true) {
+            document.getElementById(winCell1)?.classList.add("bg-red-300");
+            document.getElementById(winCell2)?.classList.add("bg-red-300");
+            document.getElementById(winCell3)?.classList.add("bg-red-300");
+            players[currentPlayer].score += 1;
+            document.getElementById(`player${currentPlayer + 1}`)!.innerHTML = `Player ${players[currentPlayer].symbol}: ${players[currentPlayer].score}`
+            currentlyPlaying = false;
+            return true;
+        };
+    }
+    return false;
 }
 
 tttBoardCreate(boardRoot);
 
-document.getElementById("resetButton")?.addEventListener("click", resetGame)
+document.getElementById("resetButton")?.addEventListener("click", resetGame);
 
 function resetGame() {
     playIndex = 0;
     document.getElementById("tttHolder")?.remove();
-    tttBoardCreate(4);  
+    tttBoardCreate(3);
+    currentlyPlaying = true;
+    players[0].cells = [];
+    players[1].cells = [];
 }
